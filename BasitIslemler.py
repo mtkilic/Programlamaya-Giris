fe63ee6541b175a5d4e2ff426480b31fe14de02b
mtkilic <kilicmahmut@ymail.com>
2017-01-15 19:01:12 -0500

    Python ile Basit islemleri ekrana basmak

---
+++ BasitIslemler.py
@@ -0,0 +1,8 @@
+birinci_sayi = int(input("Lutfen birinci sayiyi giriniz: "))
+ikinci_sayi = int(input("Lutfen ikinci sayiyi giriniz: "))
+
+print("Toplam: {}".format(birinci_sayi + ikinci_sayi))
+print("fark: {}".format(birinci_sayi - ikinci_sayi))
+print("carpim: {}".format(birinci_sayi * ikinci_sayi))
+print("bolum: {}".format(birinci_sayi / ikinci_sayi))
+print("kalan: {}".format(birinci_sayi % ikinci_sayi))
